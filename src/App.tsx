370b428daa2a78ac1bbc21f9eaa4f0253c597f87
derabola <adu.deraj@gmail.com>
2025-08-07 17:41:18 +0100

    tailwind theme

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-primary">
+        Hello world!
+      </h1>
+
+      <p className="text-red-500 p-4 ">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div className="bg-white border p-4 rounded-[var(--radius-md)] shadow-[var(--shadow-lg)]">
+        Clean card with custom design tokens
+      </div>
+    </>
+  );
+}
+
+export default App;
